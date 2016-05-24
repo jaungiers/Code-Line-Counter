8985be4b00307978969384d89db8c61c296ab645
Jakob Aungiers <jakob@jakob-aungiers.com>
2016-05-24 10:56:46 +0100

    Adding code and updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Code-Line-Counter
-Python script for iterating directory and outputting total line counts for all file types
+# Code Line Counter
+Python script for iterating directory and outputting total line counts for all file types.
+
+## Useage Example
+`python code-line-counter.py "C:\WORK\Some-Project"`
---
+++ code-line-counter.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from sys import stdout
+
+rootdir = sys.argv[1]
+extDic = {}
+
+def countLines(fPath):
+	count = 0
+	with open(fPath) as f:
+		for i, l in enumerate(f):
+			pass
+			count = i
+	return count + 1
+
+def outTab(text):
+	padSize = 10
+	padChar = " "
+	if len(str(text)) >= 10:
+		stdout.write(text)
+	else:
+		pad = ""
+		for p in xrange(padSize - len(str(text))):
+			pad += padChar
+		stdout.write(str(text) + pad)
+
+for root, subdirs, files in os.walk(rootdir):
+	for file in files:
+		fPath = root + "\\" + file;
+		fName, fExt = os.path.splitext(fPath)
+		if fExt in extDic.keys():
+			extDic[fExt] += countLines(fPath)
+		else:
+			extDic[fExt] = countLines(fPath)
+
+outTab('FILE TYPE')
+stdout.write(' | ')
+outTab('LINE COUNT')
+stdout.write('\n')
+for ext, lCount in extDic.iteritems():
+	outTab(ext)
+	stdout.write(' | ')
+	outTab(lCount)
+	stdout.write('\n')
